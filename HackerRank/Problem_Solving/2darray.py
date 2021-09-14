492a44573547aaef1bde50f272a0531c51d2672d
rajthilakam <rajthilakam@gmail.com>
2021-09-13 20:34:26 -0700

    Arrays

--- HackerRank/Problem_Solving/2darray.py
+++ HackerRank/Problem_Solving/2darray.py
@@ -19,7 +19,7 @@ maxnumber = 0
 
 for i in range(4):
     for j in range(4):
-        a = arr[i][j]+arr[i][j+1]+arr[i][j+2]+arr[i+1][j+1]+arr[i+2][j]+arr[i+2][j+1]+              arr[i+2][j+2]
+        a = arr[i][j]+arr[i][j+1]+arr[i][j+2]+arr[i+1][j+1]+arr[i+2][j]+arr[i+2][j+1]+arr[i+2][j+2]
     
         if (maxnumber < a or (i==0 and j==0)):
             maxnumber = a
