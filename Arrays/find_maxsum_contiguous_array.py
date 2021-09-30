3128f2b52b79087dd143e941b0892ce2a20f7a11
rajthilakam <rajthilakam@gmail.com>
2021-09-29 23:01:15 -0700

    Find_maxsum

---
+++ Arrays/find_maxsum_contiguous_array.py
@@ -0,0 +1,25 @@
+#Time complexity o(n^2)
+arr = [-1,4,5,-2]
+
+def sumarray(arr):
+    max_sum = 0
+    for i in range(len(arr)):
+        curr_sum = arr[i]
+       
+        j=i+1
+        while j<=len(arr):
+             
+          if j == len(arr):
+            break         
+          curr_sum = curr_sum+arr[j]
+          j+=1
+         
+          if(max_sum < curr_sum):
+              max_sum = curr_sum
+        
+            
+    print (max_sum)        
+    return max_sum
+       
+      
+sumarray(arr)  
